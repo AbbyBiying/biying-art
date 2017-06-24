ad7159af9a0dd4a876faf58c789f2211047e3e7b
AbbyBiying <abbyzhangnyc@gmail.com>
2017-06-23 23:11:23 -0700

    sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Biying Art';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MdButtonModule, MdCheckboxModule} from '@angular/material';
+import 'hammerjs';
+import { MaterialModule, MdNativeDateModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MdMenuModule} from '@angular/material';
+import { HttpModule} from '@angular/http';
+import { MdSidenavModule} from '@angular/material';
+import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    HttpModule,
+    MaterialModule,
+    MdNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
