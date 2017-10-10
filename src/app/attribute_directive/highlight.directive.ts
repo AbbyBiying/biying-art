ec61579306c28c7c308f043012ffdfc5e82bf187
Abby <ABBYZHANGNYC@GMAIL.COM>
2017-10-09 20:32:54 -0700

    refactor directive

--- src/app/attribute_directive/highlight.directive.ts
+++ src/app/attribute_directive/highlight.directive.ts
@@ -1,14 +1,17 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
-@Directive({ selector: '[artHighlight]' })
-
+@Directive({ 
+	selector: '[artHighlight]' 
+})
 
 export class HighlightDirective {
+	
+	@Input('artHighlight') highlightColor: string;
+
   constructor(private el: ElementRef) { }
-	 
   
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
+ 		this.highlight(this.highlightColor || 'gold');
   }
  
   @HostListener('mouseleave') onMouseLeave() {
@@ -17,5 +20,5 @@ export class HighlightDirective {
  
   private highlight(color: string) {
     this.el.nativeElement.style.backgroundColor = color;
-}
+	}
 }
