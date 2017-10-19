994ffd934f43adf6b598c7d7fabf64ae6df1233c
Abby <ABBYZHANGNYC@GMAIL.COM>
2017-10-18 23:42:57 -0700

    change dropdown menu

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,12 +6,24 @@ import { artAnimations } from '../animations/art-animations';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss'],
   animations: artAnimations,
+
 })
 export class PortfolioComponent implements OnInit {
 	color: string;
-	
+	selectedCodeValue: string;
   constructor() { }
 
   ngOnInit() {
   }
+  openLink(){
+    if (this.selectedCodeValue == "ABZ-EXCHANGE"){
+    	window.open("https://github.com/AbbyBiying/ABZ_EXCHANGE","ABZ-EXCHANGE");
+		};
+    if (this.selectedCodeValue == "PAIRING"){
+    	window.open("https://github.com/AbbyBiying/pairing","Pairing");
+		};
+    if (this.selectedCodeValue == "Biying-Art"){
+    	window.open("https://github.com/AbbyBiying/biying-art","Biying-Art");
+		};  
+	}
 }
